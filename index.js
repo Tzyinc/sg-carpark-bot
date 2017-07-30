b67d958298141a5e6b532a0ece0cf6830ac69b50
Tzyinc <tzyinc@hotmail.com>
2017-07-30 16:16:26 +0800

    added telegram bot

--- index.js
+++ index.js
@@ -1,2 +1,93 @@
+var fs = require("fs");
 var apiKeys = JSON.parse(fs.readFileSync('apiKeys.json', 'utf8'));
 var rates = JSON.parse(fs.readFileSync('rates.json', 'utf8'));
+var TelegramBot = require('node-telegram-bot-api'),
+  bot = new TelegramBot(apiKeys.telegramKey, { polling: true });
+var n_nearest = 5;
+bot.on('message', (msg) => {
+  //console.log(msg);
+  if(msg.text){
+    if (msg.text.toLowerCase().indexOf("/start") === 0) {
+      handleStart(msg);
+    }
+    if (msg.text.toLowerCase().indexOf("/info") === 0) {
+      handleInfo(msg);
+    }
+    if (msg.text.toLowerCase().indexOf("/test") === 0) {
+      handleTest(msg);
+    }
+  }
+  if(msg.location){
+    getLocation(msg, n_nearest);
+  }
+});
+function getLocation(msg, n){
+  bot.sendMessage(msg.chat.id,"Calculating...");
+  var userLat = msg.location.latitude
+  var userLng = msg.location.longitude
+  var topn = [];
+  for (var i=0; i<rates.length; i++){
+    rate = rates[i]
+    var latDiff = rate.Location_Lat - userLat;
+    var lngDiff = rate.Location_Lng -userLng;
+    var eucDist = Math.sqrt((latDiff * latDiff) + (lngDiff * lngDiff));
+    topn.push({eucDist: eucDist, rate: rate})
+
+    topn.sort(function(a, b) {
+        return parseFloat(a.eucDist) - parseFloat(b.eucDist);
+    });
+    while (topn.length>n){
+      topn.pop();
+    }
+  }
+  var toSend = "*The ";
+  toSend += n;
+  toSend += " nearest carparks are: *\n";
+  for(var i=0; i<topn.length; i++) {
+    toSend += "*";
+    toSend += topn[i].rate.CarPark;
+    toSend += "*";
+    toSend += "\n`Weekdays: `\n";
+    toSend += topn[i].rate.WeekDays_Rate_1;
+    if (topn[i].rate.WeekDays_Rate_2 != "-") {
+      toSend += "\n"
+      toSend += topn[i].rate.WeekDays_Rate_2;
+    }
+    if (topn[i].rate.Saturday_Rate!= "-") {
+      toSend += "\n`Saturdays: `\n";
+      toSend += topn[i].rate.Saturday_Rate;
+    }
+    if (topn[i].rate.Sunday_PublicHoliday_Rate!= "-") {
+      toSend += "\n`Sun and PH: `\n";
+      toSend += topn[i].rate.Sunday_PublicHoliday_Rate;
+    }
+    toSend += "\n"
+    //"WeekDays_Rate_1":"Daily: $1.30 / 30 Mins","WeekDays_Rate_2":"-","Saturday_Rate":"-","Sunday_PublicHoliday_Rate":"-"
+  }
+
+  bot.sendMessage(msg.chat.id,toSend,{parse_mode : "Markdown"});
+}
+
+function handleStart(msg){
+  var toSend = "Hello! Send me your location (paperclip logo, bottom right) and I'll find the "
+  toSend += n_nearest
+  toSend += "nearest carparks to you, with their rates! \n \n"
+  toSend += "Developed for public use by *Ten Zhi-Yang* July 2017"
+  bot.sendMessage(msg.chat.id,toSend,{parse_mode : "Markdown"});
+}
+function handleInfo(msg){
+  var toSend = "Sg-carparks-bots databanks updated at: \n";
+  toSend += "`30 July 2017`. \n \n";
+  toSend += "Data of exactly *";
+  toSend += rates.length;
+  toSend += "* carparks have been collated from mytransport.sg datamall. \n \n"
+  toSend += "Developed for public use by *Ten Zhi-Yang*"
+  bot.sendMessage(msg.chat.id,toSend,{parse_mode : "Markdown"});
+}
+
+function handleTest(msg){
+  var toSend = "";
+  for (var i=0; i< rates.length; i++){
+    //console.log(i, rates[i].CarPark)
+  }
+}
--- update-csv.js
+++ update-csv.js
@@ -5,7 +5,7 @@ var gMaps = require('@google/maps').createClient({
   key: apiKeys.gMapsKey,
   Promise: Promise // 'Promise' is the native constructor.
 });
-var toSleep = 25;
+var toSleep = 50;
 
 var fileObj = {
     filename: 'CarParkRates.csv'
@@ -19,10 +19,13 @@ var parseCallback = function(err, json) {
 function findCoordViaMap(carparks){
   var promises = [];
   var computedCarparks = []
-  for (var i=0; i < 3 /*carparks.length*/; i++) {
-    var newPromise = gMaps.geocode({address: carparks[i].CarPark, region: 'sg'}).asPromise()
-    promises.push(newPromise);
-    sleep(toSleep);
+  for (var i=0; i < carparks.length; i++) {
+    if(carparks[i].CarPark != ""){
+      var newPromise = gMaps.geocode({address: carparks[i].CarPark, region: 'sg'}).asPromise()
+      promises.push(newPromise);
+      //console.log("request number ", i, " made");
+      sleep(toSleep);
+    }
   }
 
   Promise.all(promises).then(values => {
@@ -31,21 +34,26 @@ function findCoordViaMap(carparks){
       for (var j=0; j<carparks.length; j++){
         var carpark = carparks[j]
         if(carpark.CarPark === result.query.address){
-          computedCarparks.push(getCarparkJson(carpark, result));
+          var newJson = getCarparkJson(carpark, result);
+          if (!isEmpty(newJson)){
+            computedCarparks.push(newJson);
+          }
         }
       }
     }
     //console.log(computedCarparks)
     //write json to new file
     writeToFile(computedCarparks);
-  });
+  }).catch(function(err) {
+    console.log(err); // some coding error in handling happened
+  });;
 
 }
 
 function writeToFile(carparks){
   var writeObj = {
     filename: "rates.json",
-    json: carparks
+    json: arrUnique(carparks)
   };
 
   var writeCallback = function(err) {
@@ -59,16 +67,22 @@ function writeToFile(carparks){
 }
 
 function getCarparkJson(carpark, result) {
-  return {
-    CarPark: carpark.CarPark,
-    Category: carpark.Category,
-    Address: result.json.results[0].formatted_address,
-    Location_Lat: result.json.results[0].geometry.location.lat,
-    Location_Lng: result.json.results[0].geometry.location.lng,
-    WeekDays_Rate_1: carpark.WeekDays_Rate_1,
-    WeekDays_Rate_2: carpark.WeekDays_Rate_2,
-    Saturday_Rate: carpark.Saturday_Rate,
-    Sunday_PublicHoliday_Rate: carpark.Sunday_PublicHoliday_Rate,
+  if (result.json.results.length>0){
+    return {
+      CarPark: carpark.CarPark,
+      Category: carpark.Category,
+      Address: result.json.results[0].formatted_address,
+      Location_Lat: result.json.results[0].geometry.location.lat,
+      Location_Lng: result.json.results[0].geometry.location.lng,
+      WeekDays_Rate_1: carpark.WeekDays_Rate_1,
+      WeekDays_Rate_2: carpark.WeekDays_Rate_2,
+      Saturday_Rate: carpark.Saturday_Rate,
+      Sunday_PublicHoliday_Rate: carpark.Sunday_PublicHoliday_Rate,
+    };
+  }else{
+    console.log(result);
+    return {
+    };
   }
 }
 
@@ -82,4 +96,27 @@ function sleep(milliseconds) {
       break;
     }
   }
+}
+
+function isEmpty(obj) {
+    return JSON.stringify(obj) === JSON.stringify({});
+}
+
+function arrUnique(obj){
+    var uniques=[];
+    var stringify={};
+    for(var i=0;i<obj.length;i++){
+       var keys=Object.keys(obj[i]);
+       keys.sort(function(a,b) {return a-b});
+       var str='';
+        for(var j=0;j<keys.length;j++){
+           str+= JSON.stringify(keys[j]);
+           str+= JSON.stringify(obj[i][keys[j]]);
+        }
+        if(!stringify.hasOwnProperty(str)){
+            uniques.push(obj[i]);
+            stringify[str]=true;
+        }
+    }
+    return uniques;
 }
